f40f60bf9df61754ef43cf7325e00a5f3541aed1
Semen Zemlyakov <sema_zemlyakov@mail.ru>
2022-12-26 20:56:41 +0300

    Commit 1

---
+++ laba9vvp/laba9vvp/laba9vvp.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+#include "sdt.h"
+using namespace std;
+int main()
+{
+	int a, b;
+	cout << "Enter A and B: ";
+	cin >> a >> b;
+	cout << "A + B = " << a + b << endl;
+	return 0;
+   
+}
